c40cf63bbd6eedfea17a6ceff7d5291a4da7c5fb
Sai Narsimha Reddy Keesari <k.sainarsimhareddy03@gmail.com>
2024-04-16 16:30:18 -0500

    changes

--- server/app.js
+++ server/app.js
@@ -14,10 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api", paymentRoute);
 // app.use('/api/auth', require('./routes/auth'));
 app.use('/api/auth', auth);
-app.get('/', (req, res) => {
-  // Handle the GET request for /api/announcement here
-  res.send("You're good to go!");
-});
+
 app.get("/api/getkey", (req, res) =>
   res.status(200).json({ "key": "rzp_test_j9in5uw2tvdckP"})
 );
--- server/server.js
+++ server/server.js
@@ -10,7 +10,9 @@ export const instance = new Razorpay({
 });
 
 const port = process.env.PORT || 3000;
-
-app.listen(port, () =>
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+app.listen(process.env.PORT, () =>
   console.log(`Server is working on ${process.env.PORT}`)
 );
