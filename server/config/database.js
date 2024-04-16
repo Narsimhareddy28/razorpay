cb7aaabe6e3cfbfff71920fa7ad18491ac713e01
Sai Narsimha Reddy Keesari <k.sainarsimhareddy03@gmail.com>
2024-04-16 17:44:36 -0500

    minor changes

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  const { connection } = await mongoose.connect("mongodb://127.0.0.1:27017/razor");
+  const { connection } = await mongoose.connect("mongodb+srv://ksainarsimhareddy03:Parasfstorm28@cluster0.rkprzs7.mongodb.net/");
   console.log(`Mongodb is connected with ${connection.host}`);
 };
